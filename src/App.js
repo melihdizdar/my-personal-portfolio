39266b9e95c425161e55648ec2c872acdccd1a79
Melih Dizdar <mlhdzdr@gmail.com>
2022-03-16 03:41:57 +0300

    added footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Resume from "./components/Resume";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Projects/>
         <Contact/>
       </main>
+      <Footer/>
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className="uk-section-xsmall footer">
+            <div className="uk-container">
+                <div className="uk-text-center uk-light">
+                    <div> Made with UIkit and React</div>
+                    <div>Copyright 2022 © All rights reserved</div>
+                </div>
+            </div>
+        </footer>
+    )
+}
